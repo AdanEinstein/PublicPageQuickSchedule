6f17f96332640183e4b14cc3c01c5965507df024
AdanEinstein <adaneinstein@gmail.com>
2022-06-25 15:06:34 -0300

    Tela download

--- pages/download.tsx
+++ pages/download.tsx
@@ -1,13 +1,46 @@
-import Layout from "../components/layout/Layout"
+import Link from "next/link";
+import Layout from "../components/layout/Layout";
+
+// Windows https://drive.google.com/u/0/uc?id=1ICLEz_OWwnDVKWhViZE9xtXbG30oYXFV&export=download
+// MacOS https://drive.google.com/u/0/uc?id=16AN71VlXf5Ukr9XwfMh-NSPEG7o0YQ84&export=download
 
 const Download: React.FC = () => {
-    return (
-        <Layout page="Download">
-            <div className="container h-100 d-flex align-items-center">
-                
-            </div>
-        </Layout>
-    )
-}
+	return (
+		<Layout page="Download">
+			<div className="container h-100 d-flex flex-column flex-sm-row justify-content-center align-items-center justify-content-sm-around">
+				<div>
+					<h1>Efetue o donwload aqui!</h1>
+					<p>Um ótimo sistema de agenda e finanças para para você!</p>
+					<p>
+						Funciona tanto em sistema operacional{" "}
+						<strong className="text-success">Windows</strong> quanto{" "}
+						<strong className="text-info">MacOS</strong>
+					</p>
+					<h4>Aproveite!</h4>
+				</div>
+				<div className="d-flex">
+					<Link
+						href={
+							"https://drive.google.com/u/0/uc?id=1ICLEz_OWwnDVKWhViZE9xtXbG30oYXFV&export=download"
+						}
+					>
+						<a className="btn btn-lg btn-success m-1">
+							Windows <i className="bi bi-windows"></i>
+						</a>
+					</Link>
+					<Link
+						href={
+							"https://drive.google.com/u/0/uc?id=16AN71VlXf5Ukr9XwfMh-NSPEG7o0YQ84&export=download"
+						}
+					>
+						<a className="btn btn-lg btn-info m-1">
+							MacOS <i className="bi bi-apple"></i>
+						</a>
+					</Link>
+				</div>
+			</div>
+		</Layout>
+	);
+};
 
-export default Download
+export default Download;
