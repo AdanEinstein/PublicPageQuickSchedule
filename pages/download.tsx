66d4a334f1f80b1dcc4a309796d1366e9e72a58c
AdanEinstein <adaneinstein@gmail.com>
2022-06-25 16:12:42 -0300

    Tela download

--- pages/download.tsx
+++ pages/download.tsx
@@ -1,16 +1,25 @@
 import Link from "next/link";
+import { useState } from "react";
+import { Button } from "react-bootstrap";
 import Layout from "../components/layout/Layout";
 
-// Windows https://drive.google.com/u/0/uc?id=1ICLEz_OWwnDVKWhViZE9xtXbG30oYXFV&export=download
-// MacOS https://drive.google.com/u/0/uc?id=16AN71VlXf5Ukr9XwfMh-NSPEG7o0YQ84&export=download
+// Windows
+const urlWindows =
+	"https://mega.nz/file/mfIhjK4J#bwaE4GOnd7hsP1zvgNsU-KJUohzMTdoECUfIAY3Sjro";
+// MacOS
+const urlMac =
+	"https://mega.nz/file/6WRQWI7L#jsafMEb9xmT4_RPWEZpd1vfw2QIjjZdxdQVLhzMXcSo";
 
 const Download: React.FC = () => {
+	// const [download, setDownload] = useState<string>();
 	return (
 		<Layout page="Download">
 			<div className="container h-100 d-flex flex-column flex-sm-row justify-content-center align-items-center justify-content-sm-around">
 				<div>
 					<h1>Efetue o donwload aqui!</h1>
-					<p className="fs-5">Um ótimo sistema de agenda e finanças para para você!</p>
+					<p className="fs-5">
+						Um ótimo sistema de agenda e finanças para para você!
+					</p>
 					<p className="fs-5">
 						Funciona tanto em sistema operacional{" "}
 						<strong className="text-success">Windows</strong> quanto{" "}
@@ -20,25 +29,29 @@ const Download: React.FC = () => {
 				</div>
 				<div className="d-flex">
 					<Link
-                        target={"_blank"}
-						href={
-                            "https://drive.google.com/u/0/uc?id=1ICLEz_OWwnDVKWhViZE9xtXbG30oYXFV&export=download"
-						}
+						href={urlWindows}
                         >
-						<a className="btn btn-lg btn-success m-1">
+						<a target={"_blank"} className="btn btn-lg btn-success m-1">
 							Windows <i className="bi bi-windows"></i>
 						</a>
 					</Link>
 					<Link
-                        target={"_blank"}
-						href={
-							"https://drive.google.com/u/0/uc?id=16AN71VlXf5Ukr9XwfMh-NSPEG7o0YQ84&export=download"
-						}
+						href={urlMac}
 					>
-						<a className="btn btn-lg btn-info m-1">
+						<a target={"_blank"} className="btn btn-lg btn-info m-1">
 							MacOS <i className="bi bi-apple"></i>
 						</a>
 					</Link>
+					{/* <Button size={"lg"} variant={"success"} className="m-1"
+						onClick={() => setDownload(urlWindows)}
+						>
+						Windows <i className="bi bi-windows"></i>
+					</Button>
+					<Button size={"lg"} variant={"info"} className="m-1"
+						onClick={() => setDownload(urlMac)}
+					>
+						MacOS <i className="bi bi-apple"></i>
+					</Button> */}
 				</div>
 			</div>
 		</Layout>
